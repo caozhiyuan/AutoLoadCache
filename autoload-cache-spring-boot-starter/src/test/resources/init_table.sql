3cb87d7c0c65abcb6b58ec227aa25f2e997db3d0
Jeffrey.Cao <Jeffrey.Cao@budweiserapac.com>
2025-07-31 17:26:46 +0800

    * Upgrade AutoLoadCache to support Spring Boot 3.5.x with Java 17

--- autoload-cache-spring-boot-starter/src/test/resources/init_table.sql
+++ autoload-cache-spring-boot-starter/src/test/resources/init_table.sql
@@ -1,1 +1,4 @@
-create table if not exists user(id int not null primary key auto_increment, name varchar(100), password varchar(10), status int);
+create table if not exists `user`(id int not null primary key auto_increment,
+                                name varchar(100),
+                                password varchar(10),
+                                status int);
